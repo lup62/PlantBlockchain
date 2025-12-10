db1b95c9b78acc1e9295e37e250fb5d65babb936
lup62 <132756055+lup62@users.noreply.github.com>
2025-12-10 20:04:26 +0100

    aggiunti modifiers2

--- Blockchain/contracts/VarietyLicenseRegistry.sol
+++ Blockchain/contracts/VarietyLicenseRegistry.sol
@@ -18,43 +18,58 @@ contract VafietyLicenseRegistry {
   mapping (uint256 => uint256[]) private varietyToLicenses; //mappatura delle licenze per ogni varietà
   mapping (address => bool) private authorizedInspectors; //mappatura degli ispettori autorizzati
 
-  //modificatori
+  /*
+    =========MODIFIER==========
+  */
+
+  //SOLO AUTHORITY
   modifier onlyAuthority() {
     require(msg.sender == authority, "Only authority can call this function");
     _;
   }
 
+  //SOLO ISPETTORI AUTORIZZATI
   modifier onlyInspector() {
     require(authorizedInspectors[msg.sender], "Only authorized inspectors can call this function");
     _;
   }
 
+  //SOLO BREEDER DELLA VARIETÀ
   modifier onlyBreederOf(uint256 _varietyId) {
     require(msg.sender != authority, "Authority cannot be a breeder");
     require(varieties[_varietyId].breeder == msg.sender, "Only breeder of this variety can call this function");
     _;
   }
 
+  //SOLO LICENZATARIO
   modifier onlyLicenseeOf(uint256 _licenseId) {
     require(msg.sender != authority, "Authority cannot be a licensee");
     require(licenses[_licenseId].licensee == msg.sender, "Only licensee of this license can call this function");
     _;
   }
 
+  //ESISTENZA VARIETÀ, BATCH, LICENZA
   modifier varietyExists(uint256 _varietyID) {
     require(_varietyID > 0 && _varietyID <= batchCounter, "Invalid batch ID");
     _;
   }
-
   modifier batchExists(uint256 _batchId) {
     require(_batchId > 0 && _batchId <= batchCounter, "Invalid batch ID");
     _;
-}
+  }
+  modifier licenseExists(uint256 _licenseId) {
+    require(_licenseId > 0 && _licenseId <= varietyCounter, "Invalid license ID");
+    _;
+  }
 
 
 
 
-  //costruttore
+
+
+
+
+  //CONSTRUCTOR
   constructor() {
     authority = msg.sender; //l'authority è colui che deploya il contratto
     varietyCounter = 0;
