563dc9633203e3ba5ee4a7f70590b42d1f60bd0d
lup62 <oronzofranchini7@gmail.com>
2025-12-21 15:11:37 +0100

    modifica struttura hardhat

--- Blockchain/hardhat.config.js
+++ Blockchain/hardhat.config.js
@@ -1,4 +1,13 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-toolbox");
+
 module.exports = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 };
